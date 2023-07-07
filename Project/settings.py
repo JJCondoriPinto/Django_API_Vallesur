6c6ce2dc1872ba466f78a4d93a88e518d73b9ea5
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-07-07 15:00:17 -0500

    host

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%=ev4#&#xk87$j_!9v$y6u*k6c+pcft6bi^+5$fp00q=sh3fv7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
