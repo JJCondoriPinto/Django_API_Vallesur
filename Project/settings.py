3553a62e2dc466738877da9d51687f5f4cdc337d
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-07-07 14:58:21 -0500

    setting false

--- Project/settings.py
+++ Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%=ev4#&#xk87$j_!9v$y6u*k6c+pcft6bi^+5$fp00q=sh3fv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
